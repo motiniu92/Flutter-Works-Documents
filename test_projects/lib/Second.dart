f1c24c615093c776b72779590f0c35321001618a
motiniu92 <motiniu92@gmail.com>
2024-04-23 17:43:50 +0600

    text overflow

--- test_projects/lib/Second.dart
+++ test_projects/lib/Second.dart
@@ -12,16 +12,46 @@ class Second extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Container(
-          child: ElevatedButton(
-              child: Text(
-                "Go to first screen", style: TextStyle(color: Colors.red, fontSize: 25, fontStyle: FontStyle.normal),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 150,
+                width: 400,
+                child: ElevatedButton(
+                    child: Text(
+                      "Go to first screen",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 25,
+                          fontStyle: FontStyle.normal),
+                    ),
+                    onPressed: () {
+                      Get.to(First());
+                    }),
               ),
-              onPressed: (){
-                Get.to(First());
-              }
-          ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 250,
+              color: Colors.black12,
+              child: Text(
+                "Hello Delopers What is the problem, don't wary each problem have a solution in the programing sector",
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                    fontStyle: FontStyle.normal),
+                overflow: TextOverflow.ellipsis,
+                // I used ellipsis, but it works with others (fade, clip, etc.)
+                maxLines: 3,
+              ),
+            )
+          ],
         ),
       ),
     );
